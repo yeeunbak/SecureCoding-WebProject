a5850287357e88338db1b1921a48e24c5ac97c44
Yeeun <yeeunb02@gmail.com>
2026-04-23 10:17:00 +0900

    Login & Logout Controller 주석 추가

--- WebProject/src/main/java/member/LoginController.java
+++ WebProject/src/main/java/member/LoginController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+/* 로그인 버튼 클릭 시 */
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -19,32 +20,37 @@ public class LoginController extends HttpServlet {
         memberService = new MemberServiceImpl();
     }
 
+    /* /login 주소 요청 -> login.jsp 이동 */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.sendRedirect(request.getContextPath() + "/login.jsp");
     }
 
+    /* login */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         request.setCharacterEncoding("UTF-8");
 
+        /* login.jsp에서 입력한 값 꺼내기 */
         String userId = request.getParameter("userId");
         String userPw = request.getParameter("userPw");
 
+        /* DTO에 담기 */
         MemberDTO member = memberService.login(userId, userPw);
 
+        /* 회원정보 있음 */
         if (member != null) {
             HttpSession session = request.getSession();
             session.setAttribute("loginId", member.getUserId());
             session.setAttribute("loginName", member.getUserName());
             session.setAttribute("loginRole", member.getUserRole());
 
-            response.sendRedirect(request.getContextPath() + "/main.jsp");
+            response.sendRedirect(request.getContextPath() + "/main.jsp"); /* 로그인 성공 -> 메인화면 */
         } else {
-            response.sendRedirect(request.getContextPath() + "/login.jsp?msg=fail");
+            response.sendRedirect(request.getContextPath() + "/login.jsp?msg=fail"); /* 로그인 실패 */
         }
     }
 }
--- WebProject/src/main/java/member/LogoutController.java
+++ WebProject/src/main/java/member/LogoutController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+/* 로그아웃 버튼 클릭 시 */
 @WebServlet("/logout")
 public class LogoutController extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -17,8 +18,10 @@ public class LogoutController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+    		/* 현재세션 가져오기 */
         HttpSession session = request.getSession(false);
 
+        /* 세션 존재 -> loginId, loginName, loginRole 제거 */
         if (session != null) {
             session.invalidate();
         }
