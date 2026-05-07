693c2f2b6dac020b4b0bfebff8e5b9fe065d2bde
Yeeun <yeeunb02@gmail.com>
2026-05-07 16:22:50 +0900

    boardList, adminboardList -> jsp만

--- WebProject/src/main/webapp/js/board/boardList.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const moveButtons = document.querySelectorAll(".move-btn");
-
-    moveButtons.forEach(function (button) {
-        button.addEventListener("click", function () {
-            location.href = button.dataset.url;
-        });
-    });
-});
